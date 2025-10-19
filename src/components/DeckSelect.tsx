adab1bb04526899d789347eb138e0f0bde07444b
Saotalangim161099 <Saotalangim161099@gmail.com>
2025-10-19 05:46:30 -0400

    fix: change the import type and CSS part

--- src/components/DeckSelect.tsx
+++ src/components/DeckSelect.tsx
@@ -1,4 +1,4 @@
-import { Deck } from "../data/decks";
+import type { Deck } from "../data/decks";
 
 type Props = {
     decks: Deck[];
@@ -8,19 +8,18 @@ type Props = {
 
 export default function DeckSelect({ decks, activeDeckId, onChange }: Props) {
     return (
-        return (
-    <div>
-      <label className="small" htmlFor="deck">Deck</label><br />
-      <select
-        id="deck"
-        className="select"
-        value={activeDeckId}
-        onChange={(e) => onChange(e.target.value)}
-      >
-        {decks.map(d => (
-          <option key={d.id} value={d.id}>{d.title}</option>
-        ))}
-      </select>
-    </div>
+        <div>
+        <label className="small" htmlFor="deck">Deck</label><br />
+        <select
+            id="deck"
+            className="select"
+            value={activeDeckId}
+            onChange={(e) => onChange(e.target.value)}
+        >
+            {decks.map(d => (
+            <option key={d.id} value={d.id}>{d.title}</option>
+            ))}
+        </select>
+        </div>
   );
 }
